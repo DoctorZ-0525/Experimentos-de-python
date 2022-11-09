f2b69e1150c464c930a52cb8c71f63db9b8b4098
Julian Almario <102930875+DoctorZ-0525@users.noreply.github.com>
2022-11-08 19:39:01 -0500

    loops update

--- loops.py
+++ loops.py
@@ -1,10 +1,14 @@
 #loops utilizacion del for
-
+#VAR que necesito
 list_nombres = ["julian","andrea","camila","camilo"]
 
+letra = input("letra del nombre que deseas buscar: ") #input (ingreso la informacon que busco)
+
 for nom in list_nombres: #por cada nombre en la lista en la lista de nombres
-    if nom.startswith("c"): #si comienza con /alguna letra
+    if nom.startswith(letra): #si comienza con /alguna letra
         print(nom) #pintar el nombre que corresponde a la letra de inicio
+    else: #si no comienza por ninguna letra
+        print(f"No comienza con la {letra}") #pinto los nombres en la lista que no empiezen con la letra requerida
 
 
 
