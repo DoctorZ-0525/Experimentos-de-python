53b3698e7d8727ce68f5b7ba1893e8483cbf1676
Julian Almario <102930875+DoctorZ-0525@users.noreply.github.com>
2022-11-08 19:33:22 -0500

    loops

---
+++ loops.py
@@ -0,0 +1,13 @@
+#loops utilizacion del for
+
+list_nombres = ["julian","andrea","camila","camilo"]
+
+for nom in list_nombres: #por cada nombre en la lista en la lista de nombres
+    if nom.startswith("c"): #si comienza con /alguna letra
+        print(nom) #pintar el nombre que corresponde a la letra de inicio
+
+
+
+#for nombre in list_nombres:
+   # num_nombre = list_nombres.index(nombre) + 1
+    #print(f"Hola nombre {num_nombre}: {nombre}") 
