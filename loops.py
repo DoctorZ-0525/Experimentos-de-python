6248a12dae084dc10de2692299fd11b97158021e
Julian Almario <102930875+DoctorZ-0525@users.noreply.github.com>
2022-11-08 21:14:57 -0500

    correct loop

--- loops.py
+++ loops.py
@@ -10,11 +10,9 @@ for nom in list_nombres: #por cada nombre en la lista en la lista de nombres
         print(nom) #pintar el nombre que corresponde a la letra de inicio
     else: #si no comienza por ninguna letra
         print(f"No comienza con la {letra}") #pinto los nombres en la lista que no empiezen con la letra requerida
-
-#for nombre in list_nombres:
-   # num_nombre = list_nombres.index(nombre) + 1
-    #print(f"Hola nombre {num_nombre}: {nombre}") 
+    
 print("\n")
+
 #DESMEMBREDOR DE TEXTO
 print("CODIGO #2")
 palabra = input("ingresa la palabra que deceas desarmar: ") #ingreso mi texto (no tengo que transformarlo porque sigue siendo un string)
