429d9ed2c1257b47feb6ca55d7bafdac901ca2d0
Julian Almario <102930875+DoctorZ-0525@users.noreply.github.com>
2022-11-14 13:32:32 -0500

    zip 2.0

--- zip.py
+++ zip.py
@@ -1,1 +1,8 @@
 #lo que hacemos aqui e convinar varias listas
+capitales = ["Berlín", "Tokio", "París", "Helsinki", "Ottawa", "Canberra"]
+paises = ["Alemania", "Japón", "Francia", "Finlandia", "Canadá", "Australia"]
+
+combinacion = zip(paises,capitales)
+
+for pais,capital in combinacion:
+    print(f"La capital de {pais} es {capital}")
