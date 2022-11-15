0423f0ebcf8df3e02fd4617c2ccb33cf14cfb483
Julian Almario <102930875+DoctorZ-0525@users.noreply.github.com>
2022-11-14 19:41:38 -0500

    random project reto

--- Random.py
+++ Random.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import *
 
 aleatoria = randint(1, 100)
 
@@ -6,3 +6,11 @@ if aleatoria <= 50:
     print("True")
 elif aleatoria >= 50:
     print("false")
+
+#selector de nombres
+
+nombres = list(input("Dime los nombres implicados separados por espacios: "))
+
+resultado = choice(nombres)
+
+print(resultado)
---
+++ projects/ramdomizer.py
@@ -0,0 +1,10 @@
+#project 2
+#ramdomizer
+
+#CONDICIONES A CUMPLIR
+# 1- Preguntar en nombre del usuario
+# 2- Decir "terngo un numero del 1 al 100 guardado cual crees que es?"
+# 3- El jujador tendra 8 intentos para este reto y por cada intento fallifo decir si esta cerca o no
+# 4- decirle al jugador si acerto el numero y en cuantos intentos lo realizo, ademas se le dara que numero era
+# 5- sino lo asierta decir que lo vuelva a intentar
+# plus+ - decirle al jujador si desea continuar jugando
