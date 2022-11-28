6ad478276d674ab6f226fab9544d4dc500452327
Docz-mini <doctorz.0525@gmail.com>
2022-11-28 17:51:47 -0500

    interacciones 3

--- interaccion.py
+++ interaccion.py
@@ -11,3 +11,19 @@ def evaluar_jujador(dado1,dado2):
         return f"La suma de tus dados es {suma_dados}, tienes buena chanse"
     else:
         return f"La suma de tus dados{suma_dados} es la suma ganadora"
+
+
+#=======================================================================#
+#reducir listas
+
+lista_numeros = [1,2,3,45,15,17,28]
+
+def reducir_lista(lista):
+    lista = list(set(lista))
+    lista.sort()
+    lista.pop(-1)
+    return lista
+
+def promedio(lista):
+    valor_medio = sum(lista)/len(lista)
+    return valor_medio
